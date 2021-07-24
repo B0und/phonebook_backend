a438060cc42d262a2e795735e3fb6873279f9594
Unbound <f1arenova1@gmail.com>
2021-07-24 23:37:23 +0300

    3.18 done

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ require("dotenv").config();
 var morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
-
 const Contact = require("./models/contacts");
-
 const app = express();
+
 app.use(cors());
 app.use(express.static("build"));
-
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(
     ":method :url :status :response-time ms - :res[content-length] :body - :req[content-length]"
   )
 );
-
 app.use(express.json());
 
 app.get(["/api/persons", "/api/contacts"], (request, response) => {
@@ -36,27 +33,36 @@ app.post(["/api/persons", "/api/contacts"], (request, response) => {
       error: "content missing",
     });
   }
-
   const contact = new Contact({
     name: body.name,
     number: body.number,
   });
 
-  contact.save().then((new_contact) => {
-    console.log("contact saved!");
-    response.json(new_contact);
-  });
+  Contact.findOneAndUpdate(
+    { name: contact.name },
+    { number: contact.number },
+    { upsert: true },
+    function (err, doc) {
+      if (err) return res.send(500, { error: err });
+      return response.send("Succesfully saved.");
+    }
+  ).catch((error) => next(error));
 });
 
-app.get(["/api/persons/:id", "/api/contacts/:id"], (request, response) => {
-  Contact.findById(request.params.id)
-    .then((contact) => {
-      response.json(contact);
-    })
-    .catch((error) => {
-      response.status(404).end();
-    });
-});
+app.get(
+  ["/api/persons/:id", "/api/contacts/:id"],
+  (request, response, next) => {
+    Contact.findById(request.params.id)
+      .then((contact) => {
+        if (contact) {
+          response.json(contact);
+        } else {
+          response.status(404).end();
+        }
+      })
+      .catch((error) => next(error));
+  }
+);
 
 app.delete(["/api/persons/:id", "/api/contacts/:id"], (request, response) => {
   Contact.findByIdAndDelete({ _id: request.params.id }, function (err) {
@@ -73,9 +79,22 @@ app.get("/info", (request, response) => {
     const message = `Phonebook has info for ${contact.length} people`;
     response.send(`<p>${message}</p> <p>${new Date()}</p> `);
   });
-
-
 });
+
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+// handler of requests with result to errors
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
